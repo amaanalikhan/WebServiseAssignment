92ed8e9bcc6c770f20e4652b557da6d245523b6f
Amaan Ali Khan <amaan.khan@SGZ.com>
2019-01-31 17:10:08 +0530

    Assignment

---
+++ WSAssignment/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.MathService obj = new localhost.MathService();
+            //int value1 = Convert.ToInt32(Console.ReadLine());
+            //int value2 = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            ////List<int> tmp = new List<int> { 1, 2, 3, 4, 5 };
+            ////Object o = tmp;
+
+            //int    r = obj.Add(value1, value2);
+
+            //string r = obj.Hello(s);
+            //double ans=obj.Hello(s);
+            Console.WriteLine(obj.AddNNumbers(s));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WSAssignment/WebService/MathService.asmx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for MathService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MathService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello ";
+        }
+        [WebMethod]
+        public string Hello(String s)
+        {
+            return "Hello " + s;
+        }
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+
+            return a + b;
+
+        }
+
+
+        [WebMethod]
+        public double AddNNumbers(String s)
+        {
+            
+            double total = 0;
+            string[] tokens = s.Split(' ');
+            foreach (string item in tokens)
+            {
+                total += double.Parse(item, CultureInfo.InvariantCulture);
+            }
+
+            //total = Convert.ToDouble(s.Split(','));
+            return total;
+
+        }
+        [WebMethod]
+        public System.Single Subtract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0)
+                return -1;
+            return Convert.ToSingle(A / B);
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
